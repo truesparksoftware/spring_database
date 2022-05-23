6c8551d81bb2bbac37c1bcb6ed98e30ccfbf8c82
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2022-05-23 21:03:53 +0530

    added project

---
+++ SpringbootInterceptor/src/main/java/com/example/demo/controller/ProductServiceController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Product;
+
+@RestController
+public class ProductServiceController {
+	private static Map<String, Product> productRepo = new HashMap<>();
+	static {
+		Product honey = new Product();
+		honey.setId("1");
+		honey.setName("Honey");
+		productRepo.put(honey.getId(), honey);
+		Product almond = new Product();
+		almond.setId("2");
+		almond.setName("Almond");
+		productRepo.put(almond.getId(), almond);
+
+
+	}
+
+	@GetMapping("/products")
+	@PostMapping("/products")
+	public ResponseEntity<Object> getProduct() {
+		return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+	}
+}
